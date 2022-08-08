ade7ea18b9d7e003694b57f249db18b3d6469790
Elena <lelya2402@rambler.ru>
2022-08-08 21:23:42 +0300

    task11

---
+++ seminar_1/task11/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+
+int GetMaxDeletefromNumber(int number3)
+{
+    int resalt=number3/100;
+    int resalt1=number3 % 10;
+    int resalt2=number3*10 + resalt1;
+
+    return resalt2;
+}
+int number3=new Random().Next(100,999);
+int maxDigit=GetMaxDeletefromNumber(number3);
+Console.WriteLine($"Из числа {number3} получили {maxDigit}");
