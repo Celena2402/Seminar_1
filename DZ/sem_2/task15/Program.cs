36aad418adbcfd2c888315f965e69af5c110f4d0
Elena <lelya2402@rambler.ru>
2022-08-09 20:43:55 +0300

    task15

--- DZ/sem_2/task15/Program.cs
+++ DZ/sem_2/task15/Program.cs
@@ -5,3 +5,17 @@
 7 -> да
 1 -> нет
 */
+
+int dayNumber = new int();
+Console.WriteLine("Введите номер дня недели");
+dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (dayNumber==6 || dayNumber==7)   //  || -значение ИЛИ, && - значение И
+{
+    Console.WriteLine("Это выходной день");
+}
+else
+{
+    Console.WriteLine("Это рабочий день");
+}
+
