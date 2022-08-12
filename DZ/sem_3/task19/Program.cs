5c546531ecda4ad6883dce8c86af50a385609bb9
Elena <lelya2402@rambler.ru>
2022-08-12 21:01:08 +0300

    task 19

--- DZ/sem_3/task19/Program.cs
+++ DZ/sem_3/task19/Program.cs
@@ -6,3 +6,17 @@
 12821 -> да
 23432 -> да
 */
+
+Console.WriteLine("Введите число не меньше пятизначного: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number>99 && Number<1000)
+{
+    //int firstNumber = Number / 100;
+    //int twoNumber = Number/10-firstNumber * 10;
+    //Console.WriteLine($"В трехзначном числе {Number} второе число получается {twoNumber}.");
+}
+else
+{
+    Console.WriteLine($"Это число меньше пятизначного. Введите другое число!");
+}
