4d8748889adda2176e66608fb44cfa21a2be0bbc
Elena <lelya2402@rambler.ru>
2022-08-12 12:27:20 +0300

    task 22 c командой Math.Pow(i, stepen)

---
+++ DZ/sem_3/task23/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int N=new int ();
+int stepen = 3;
+
+Console.Write("Введите число N = ");
+N = Convert.ToInt16(Console.ReadLine());
+for (int i=1; i<= N; i++)
+{
+     int square = Convert.ToInt32(Math.Pow(i, stepen)); //Math.Pow(i, stepen) - возведение в тепень
+       //int square=i*i*i;
+       if (i<N)
+       {
+        Console.Write($" "+square+",");
+       }
+       else
+       {
+        Console.Write(" "+Math.Pow(i, stepen));
+       //Console.Write(" "+i*i*i);
+       }
+
+}
+Console.WriteLine(" ");
