85c42be7f7242115187a475c06068618df47c801
Elena <lelya2402@rambler.ru>
2022-08-15 21:22:56 +0300

    task28

--- seminar_1/sem_4/task28/Program.cs
+++ seminar_1/sem_4/task28/Program.cs
@@ -1,9 +1,27 @@
 ﻿/*
-Задача 28: Напишите программу, которая
-принимает на вход число N и выдаёт
+Задача 28: Напишите программу, которая принимает на вход число N и выдаёт
 произведение чисел от 1 до N.
 4 -> 24
 5 -> 120
 */
 
+Console.WriteLine("Введите число:");
+//int num=int.Parse(Console.ReadLine());//Parse - преобразует одит тип данных в другой
+int num=Convert.ToInt32(Console.ReadLine());
+
+int GetProNums(int number)
+{
+    int pro=1;
+    int i=1;
+    while(i<=number)
+    {
+        //sum+=i;
+        pro=pro*i;
+        Console.Write($" {pro} ");
+        i++;
+    }
+    return pro; //возврат функции
+}
+
+Console.WriteLine($"Сумма чисел от 1 до А= {GetProNums(num)}");
 
