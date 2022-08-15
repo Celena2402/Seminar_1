93f88fd5ab9f7a904f51c595627f46ba3d5529e6
Elena <lelya2402@rambler.ru>
2022-08-15 21:55:59 +0300

    task28-30

--- seminar_1/sem_4/task28/Program.cs
+++ seminar_1/sem_4/task28/Program.cs
@@ -25,3 +25,20 @@ int GetProNums(int number)
 
 Console.WriteLine($"Сумма чисел от 1 до А= {GetProNums(num)}");
 
+/*
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int factorial(int number)
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+
+Console.WriteLine($"Факториал {factorial(num)}");
+*/
